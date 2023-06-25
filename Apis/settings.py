68822394d25bad48f5983fc80f356f95edcb832b
Mathefagbe <fagbemi65@gmail.com>
2023-06-25 21:13:01 +0100

    update requirement.text

--- Apis/settings.py
+++ Apis/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-k0lfprp#$rkn!q6^f@x#vt^z^kf#+0g3bi809m2mk$@zlk(g05"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = ['*']
 
